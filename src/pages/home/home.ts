877d02d32dfbe4425b2da6ee6f64e10c24e36463
ristra <galih.ristra@gmail.com>
2017-10-31 11:20:33 +0700

    2nd commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Observable } from "rxjs/Observable";
+import { Http } from "@angular/http";
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  accounts: Observable<any>;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public http: Http) {
+    this.http.get('http://172.18.2.137/IonicCrud/Services/WebService1.asmx/getListAccounts')
+    .map(data => data.json()).subscribe(res => {
+      this.accounts = res;
+      console.log(this.accounts);
+    })
   }
 
 }
